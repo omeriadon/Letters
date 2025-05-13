30f8b1983f56dad97ad072b6f102465bf54573e1
Adon O <adon.omeri@student.education.wa.edu.au>
2025-05-13 20:20:31 +0800

    Initial Commit

---
+++ Letters/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Letters
+//
+//  Created by Adon Omeri on 13/5/2025.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Letters/LettersApp.swift
@@ -0,0 +1,17 @@
+//
+//  LettersApp.swift
+//  Letters
+//
+//  Created by Adon Omeri on 13/5/2025.
+//
+
+import SwiftUI
+
+@main
+struct LettersApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
